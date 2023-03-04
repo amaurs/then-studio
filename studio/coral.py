918b308920fa410ebfb39d47c58f8d755ac1eb06
amaurs <amaury.gtz@gmail.com>
2023-03-03 23:46:44 -0800

    🧹 Spring cleaning.

--- studio/coral.py
+++ studio/coral.py
@@ -1,17 +1,15 @@
 import numpy
-import scipy
-from scipy import ndimage
 from scipy.spatial import distance
-import random 
+import random
 import colorsys
 import math
-import hilbert
+import imageio
+
 w = 10
 h = 10
 
 width = 4096
 height = 4096
-
 
 
 def NN(A, start):
@@ -24,117 +22,118 @@ def NN(A, start):
     cost = 0
     N = A.shape[0]
     mask = numpy.ones(N, dtype=bool)  # boolean values indicating which 
-                                   # locations have not been visited
+    # locations have not been visited
     mask[start] = False
 
-    for i in range(N-1):
+    for i in range(N - 1):
         last = path[-1]
-        next_ind = numpy.argmin(A[last][mask]) # find minimum of remaining locations
-        next_loc = numpy.arange(N)[mask][next_ind] # convert to original location
+        next_ind = numpy.argmin(A[last][mask])  # find minimum of remaining locations
+        next_loc = numpy.arange(N)[mask][next_ind]  # convert to original location
         path.append(next_loc)
         mask[next_loc] = False
         cost += A[last, next_loc]
 
     return path, cost
 
+
 def paint_pixel(image, x, y, colors):
-    print "(%s,%s)" % (x,y)
-    theStack = [ (x, y) ]
+    print("(%s,%s)" % (x, y))
+    theStack = [(x, y)]
     count = 0
     while len(theStack) > 0:
-        
+
         x, y = theStack.pop()
 
-        if sum(image[x,y]) > 0:
+        if sum(image[x, y]) > 0:
             continue
-        print x,y
-        image[x,y] = colors[count]
+        print(x, y)
+        image[x, y] = colors[count]
         count = count + 1
-        if x > 0 and y> 0 and x < width - 1 and y < height - 1:
-            theStack.append((x + random.randint(0, 1), y + random.randint(0, 1) ))  # right
-            theStack.append((x + random.randint(0, 1), y - random.randint(0, 1))) # left
-            theStack.append((x , y + 1 ))  # down
-            theStack.append( (x - random.randint(0, 1) , y - random.randint(0, 1)) )  
+        if x > 0 and y > 0 and x < width - 1 and y < height - 1:
+            theStack.append((x + random.randint(0, 1), y + random.randint(0, 1)))  # right
+            theStack.append((x + random.randint(0, 1), y - random.randint(0, 1)))  # left
+            theStack.append((x, y + 1))  # down
+            theStack.append((x - random.randint(0, 1), y - random.randint(0, 1)))
 
 
 def getRGBfromI(RGBint):
-    Blue =  RGBint & 255
+    Blue = RGBint & 255
     Green = (RGBint >> 8) & 255
-    Red =   (RGBint >> 16) & 255
-    return (Red,Green,Blue)
+    Red = (RGBint >> 16) & 255
+    return (Red, Green, Blue)
 
-def step (r,g,b, repetitions=1):
-    lum = math.sqrt( .241 * r + .691 * g + .068 * b )
- 
-    h, s, v = colorsys.rgb_to_hsv(r,g,b)
- 
+
+def step(r, g, b, repetitions=1):
+    lum = math.sqrt(.241 * r + .691 * g + .068 * b)
+
+    h, s, v = colorsys.rgb_to_hsv(r, g, b)
+
     h2 = int(h * repetitions)
     lum2 = int(lum * repetitions)
     v2 = int(v * repetitions)
- 
+
     return (h2, lum, v2)
+
 
 def order_by_hue():
     colours = []
     colours_final = []
-    for number in range(width*height):
-        rgb = getRGBfromI(number)
-        colours.append(rgb)
-    #colours.sort(key=lambda rgb: colorsys.rgb_to_hsv(*rgb) )
-    colours.sort(key=lambda (r,g,b): step(r,g,b,8)  )
+    for number in range(width * height):
+        r, g, b = getRGBfromI(number)
+        colours.append((r, g, b))
+    # colours.sort(key=lambda rgb: colorsys.rgb_to_hsv(*rgb) )
+    def helper(rgb):
+        r, g, b = rgb
+        return step(r, g, b, 8)
+    colours.sort(key=helper)
     return colours
+
 
 def order_tsp():
     colours = []
-    for number in range(1,width*height):
+    for number in range(1, width * height):
         rgb = getRGBfromI(number)
         colours.append(rgb)
 
-    colours_length = width*height
-    A = numpy.zeros([colours_length,colours_length])
-    for x in range(0, colours_length-1):
-        for y in range(0, colours_length-1):
-            A[x,y] = distance.euclidean(colours[x],colours[y])
- 
+    colours_length = width * height
+    A = numpy.zeros([colours_length, colours_length])
+    for x in range(0, colours_length - 1):
+        for y in range(0, colours_length - 1):
+            A[x, y] = distance.euclidean(colours[x], colours[y])
+
     # Nearest neighbour algorithm
     path, _ = NN(A, 0)
- 
+
     # Final array
     colours_nn = []
     for i in path:
-        colours_nn.append(  colours[i]  )
+        colours_nn.append(colours[i])
 
     return colours_nn
-def order_hilbert():
-    colours = []
-    for number in range(1,width*height):
-        rgb = getRGBfromI(number)
-        colours.append(rgb)
-    colours.sort(key=lambda (r,g,b):hilbert.Hilbert_to_int([int(r*255),int(g*255),int(b*255)])  )
-    return colours
+
 
 def spiral(N, M):
-    x,y = 0,0   
+    x, y = 0, 0
     dx, dy = 0, -1
 
-    for dumb in xrange(N*M):
-        if abs(x) == abs(y) and [dx,dy] != [1,0] or x>0 and y == 1-x:  
-            dx, dy = -dy, dx            # corner, change direction
+    for dumb in range(N * M):
+        if abs(x) == abs(y) and [dx, dy] != [1, 0] or x > 0 and y == 1 - x:
+            dx, dy = -dy, dx  # corner, change direction
 
-        if abs(x)>N/2 or abs(y)>M/2:    # non-square
-            dx, dy = -dy, dx            # change direction
-            x, y = -y+dx, x+dy          # jump
+        if abs(x) > N / 2 or abs(y) > M / 2:  # non-square
+            dx, dy = -dy, dx  # change direction
+            x, y = -y + dx, x + dy  # jump
 
         yield x, y
-        x, y = x+dx, y+dy
+        x, y = x + dx, y + dy
 
-def paint_spiral(image,colors):
 
+def paint_spiral(image, colors):
     order = spiral(height, width)
-    print order
+    print(order)
     index = 0
-    for x,y in order:
-        image[x,y] = colors[index]
+    for x, y in order:
+        image[x, y] = colors[index]
         index += 1
 
 
@@ -142,7 +141,7 @@ def getIfromRGB(rgb):
     red = rgb[0]
     green = rgb[1]
     blue = rgb[2]
-    RGBint = (red<<16) + (green<<8) + blue
+    RGBint = (red << 16) + (green << 8) + blue
     return RGBint
 
 
@@ -150,11 +149,9 @@ image_1 = numpy.zeros((height, width, 3))
 
 colors = order_by_hue()
 for i in range(100):
-    print colors[i]
-print len(colors)
-#paint_pixel(image_1, random.randint(0, width), random.randint(0, height), colors)
-paint_spiral(image_1,colors)
+    print(colors[i])
+print(len(colors))
+# paint_pixel(image_1, random.randint(0, width), random.randint(0, height), colors)
+paint_spiral(image_1, colors)
 
-scipy.misc.imsave('spiral.png', image_1)
-
-
+imageio.imwrite('spiral.png', image_1)
--- studio/pocket.py
+++ studio/pocket.py
@@ -2,38 +2,38 @@ import numpy
 import scipy
 from scipy import ndimage
 
+
 def getRGBfromI(RGBint):
-    blue =  RGBint & 255
+    blue = RGBint & 255
     green = (RGBint >> 8) & 255
-    red =   (RGBint >> 16) & 255
+    red = (RGBint >> 16) & 255
     return 255 - red, 255 - green, 255 - blue
+
 
 def getIfromRGB(rgb):
     red = rgb[0]
     green = rgb[1]
     blue = rgb[2]
-    RGBint = (red<<16) + (green<<8) + blue
+    RGBint = (red << 16) + (green << 8) + blue
 
     return RGBint
 
+
 r = 255
 
-height = 7 * 3 * 3 * 4 * 2 * 5 * 2 
+height = 7 * 3 * 3 * 4 * 2 * 5 * 2
 width = 3 * 3 * 3 * 3 * 3 * 3
 
 pocket = numpy.zeros((height, width, 3))
 
 for i in range(width):
-    g = (i * 255 / width) 
+    g = (i * 255 / width)
     for j in range(height):
         b = (j * 255 / height)
-        pocket[j,i] = getRGBfromI(j * width + i)
+        pocket[j, i] = getRGBfromI(j * width + i)
 
-myset = set( tuple(v) for m2d in pocket for v in m2d )
+myset = set(tuple(v) for m2d in pocket for v in m2d)
 
-print len(myset)
+print(len(myset))
 
 scipy.misc.imsave('pocket.png', pocket)
-
-
-
